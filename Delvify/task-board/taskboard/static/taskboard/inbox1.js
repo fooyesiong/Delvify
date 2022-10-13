c1f12007b6a0ecf20323aa56aaf5490e913993f6
fooyesiong <104306001+fooyesiong@users.noreply.github.com>
2022-10-13 18:03:13 +0800

    Add files via upload

---
+++ Delvify/task-board/taskboard/static/taskboard/inbox1.js
@@ -0,0 +1,134 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Use buttons to toggle between views
+    document.querySelector('#list').addEventListener('click', () => load_view('list'));
+    document.querySelector('#deleted').addEventListener('click', () => load_view('deleted'));
+    document.querySelector('#create').addEventListener('click', create_task);
+
+    document.querySelector('#create-form').onsubmit = create_submit;
+
+    // By default, load the lists
+    load_view('list');
+});
+
+// -----------------------------------------------------------------------------------
+function create_task() {
+    document.querySelector('#lists-view').style.display = 'none';
+    document.querySelector('#create-view').style.display = 'block';
+    document.querySelector('#task-view').style.display = 'none';
+
+    
+    // Clear out field
+    document.querySelector('#create-name').value = '';
+}
+
+function create_submit() {
+    const create_name = document.querySelector('#create-name').value;
+
+    fetch('/lists', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: create_name,
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+            load_view('list');
+        });
+
+    return false;
+}
+
+function load_view(view) {
+    
+    fetch(`/lists/${view}`)
+        .then(response => response.json())
+        .then(lists => {
+            console.log(lists)
+            for (let i of Object.keys(lists)) {
+                const elem_list = document.createElement('div');
+
+                elem_list.innerHTML = `
+                    <div>Name: ${lists[i].name}</div>
+                    <div>Date: ${lists[i].timestamp}</div>
+                    <div class="list-buttons">
+                        <button class="list-buttons" id="deleted">${lists["deleted"] ? "Undelete" : "Delete"}</button>
+                    </div>
+                `;
+                //id = lists[i].id;
+                
+                elem_list.addEventListener('click', () => load_task(lists[i].id));
+
+       
+                document.querySelector('#lists-view').append(elem_list);
+            };
+            var el = document.getElementById('deleted');
+            if(el){        
+                document.querySelector('#deleted').addEventListener('click', () => {
+                    fetch(`/lists/${id}`,{
+                        method: 'PUT',
+                        body: JSON.stringify({
+                            deleted: !lists.deleted
+                        })
+                    })
+                    // .then(response => response.json())
+                        .then(list => {
+                            // console.log(email);
+                            load_view('list');
+                        });
+                })   
+                } 
+        });
+    // Show lists view and hide other views
+    document.querySelector('#lists-view').style.display = 'block';
+    document.querySelector('#create-view').style.display = 'none';
+    document.querySelector('#task-view').style.display = 'none';
+    // Show the view name
+    document.querySelector('#lists-view').innerHTML = `<h3>${view.charAt(0).toUpperCase() + view.slice(1)}</h3>`;
+
+}
+
+
+function load_task(id) {
+    
+    document.querySelector('#task-view').style.display = 'block';
+    document.querySelector('#lists-view').style.display = 'none';
+
+    fetch(`/tasks/${id}`)
+        .then(response => response.json())
+        .then(task => {
+            console.log(task);
+            document.querySelector('#task-view').innerHTML = `
+            <div>Name: ${task.name}</div>
+            <div>Description: ${task.description}</div>
+            <div>Deadline: ${task.deadline}</div>         
+            
+            <div class="task-buttons">
+                <button class="btn-task" id="deleted">${task["deleted"] ? "Undelete" : "Delete"}</button>
+            </div>
+            <hr>
+            <div>
+                ${task.body}
+            </div>
+            
+          `;
+          var el = document.getElementById('deleted');
+          if(el){
+            document.querySelector('#deleted').addEventListener('click', () => {
+                fetch(`/tasks/${id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        deleted: !task.deleted
+                    })
+                })
+                // .then(response => response.json())
+                    .then(task => {
+                        // console.log(email);
+                        load_view('list');
+                    });
+            })
+          }    
+        })
+        
+
+}
